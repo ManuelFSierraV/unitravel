5cbd8a83f9f0b6c255eebcb12d3b4647705600af
manuelFelipe <manuelf.sierrav@uqvirtual.edu.co>
2022-03-14 18:37:37 -0500

    Configuracion inicial del proyecto

---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/persistenciaMain.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.unitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class persistenciaMain {
+    public static void main(String[]args){
+        SpringApplication.run(persistenciaMain.class,args);
+    }
+}
